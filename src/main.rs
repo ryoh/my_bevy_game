96b6f125cd7cc0905cfdcfecb9e6d1c6fcb722b5
Ryoh Kawai <kawairyoh@gmail.com>
2024-02-20 22:33:12 +0900

    fix: resource stage

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ pub struct HelloPlugin;
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, add_people)
-            .add_systems(Update, (hello_world, greet_people));
+            .add_systems(Update, greet_people);
     }
 }
 
@@ -15,28 +15,15 @@ struct Person;
 #[derive(Component)]
 struct Name(String);
 
-fn hello_world() {
-    println!("hello world!");
-}
-
 fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Elaina Proctor".to_string())));
     commands.spawn((Person, Name("Renzo Hume".to_string())));
     commands.spawn((Person, Name("Zayna Nieves".to_string())));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
+fn greet_people(time: Res<Time>, query: Query<&Name, With<Person>>) {
     for name in &query {
         println!("hello {}!", name.0);
-    }
-}
-
-fn update_people(mut query: Query<&mut Name, With<Person>>) {
-    for mut name in &mut query {
-        if name.0 == "Elaina Proctor" {
-            name.0 = "Elaina Hume".to_string();
-            break;
-        }
     }
 }
 
